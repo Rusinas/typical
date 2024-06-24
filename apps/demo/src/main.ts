d572b8031197ae2031f294c9318731a89e25e1ab
Andrew Rusinas <andrew.rusinas@gmail.com>
2024-06-24 10:48:27 +0400

    Add plausible

--- apps/demo/src/main.ts
+++ apps/demo/src/main.ts
@@ -1,5 +1,23 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+// @ts-ignore
+import { createPlausible } from 'v-plausible/vue'
 
-createApp(App).mount('#app')
+const plausible = createPlausible({
+    init: {
+        domain: 'rusinas.github.io/typical',
+        apiHost: 'https://plausible.rsns.tech',
+        trackLocalhost: false,
+    },
+    settings: {
+        enableAutoOutboundTracking: true,
+        enableAutoPageviews: true,
+    },
+    partytown: false,
+})
+
+const app = createApp(App)
+
+app.use(plausible)
+app.mount('#app')
